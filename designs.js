adde2d7047f5b738bb57caa2476f4b69d80ec49b
Abubakar Kabiru <abubakarringim@gmail.com>
2018-07-01 11:54:21 +0100

    Detailed Comments added

--- designs.js
+++ designs.js
@@ -1,6 +1,6 @@
-// Select color input
-
-
+/**
+* @description a function for addding selected color from color picker on click event.
+*/
 function addColor(){
     let col = $("td");
     col.click(function (){
@@ -14,7 +14,15 @@ function addColor(){
     })
     
  }
- // Select size input
+ 
+ 
+ /**
+* @description This function will get number of rows and colums from user and make a grid 
+         from the input made upon clicking submit button. Example: 2 height means 2 rows, 2 width means 2 columns. 
+* @param {string} height - number of rows
+* @param {string} width - number of columns
+*/
+
  $("#sizePicker").submit(function(event){
    event.preventDefault();
    
@@ -24,8 +32,15 @@ function addColor(){
    addColor();
    
  });
- // When size is submitted by the user, call makeGrid()
  
+ 
+  /**
+* @description So after size of the grid is being submitted by the user this function will make the
+                required grid based on the inputs submitted.
+* @param {string} row - number of rows
+* @param {string} col - number of columns
+*/
+
  function makeGrid(row, col){
  
   $("tr").remove();
